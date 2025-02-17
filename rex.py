dd05b2ba612689bbdd19110fcf9b218b4c12e3e8
Şevval Sağır <133015846+sevvalsagir@users.noreply.github.com>
2025-02-17 16:08:21 +0100

    Add files via upload

--- rex.py
+++ rex.py
@@ -1,1 +1,11 @@
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.datasets import load_iris
+from sklearn.model_selection import train_test_split
 
+iris = load_iris()
+x_train, x_test, y_train, y_test = train_test_split(iris.data, iris.target, test_size=0.3)
+
+model = DecisionTreeClassifier()
+model.fit(x_train, y_train)
+accuracy = model.score(x_test, y_test)
+print(f"Decision Tree Doğruluk oranı: {accuracy:.2f}")
